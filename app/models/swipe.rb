7b5c6de1242bde72f671805e021b5046dc8ba170
gauravrakheja <gauravrakheja00@gmail.com>
2021-11-05 14:14:05 +0530

    adds daily limit for number of swipes

--- app/models/swipe.rb
+++ app/models/swipe.rb
@@ -11,14 +11,29 @@ class Swipe < ApplicationRecord
 
   enum direction: { left: 0, right: 1 }
 
-  validate :from_and_to_different
-
   validates :to,
             uniqueness: { scope: :from, message: 'user is already swiped' }
 
+  validate :from_and_to_different
+
+  validate :under_daily_limit,
+           on: :create
+
+  scope :in_last_day, -> { where('created_at >= ?', Time.zone.now - 24.hours) }
+
   private
 
+  def under_daily_limit
+    return unless Swipe.where(from: from).in_last_day.count >= ENV.fetch('DEFAULT_DAILY_SWIPE_LIMIT').to_i
+
+    errors.add(:base, 'daily limit crossed')
+    throw(:abort)
+  end
+
   def from_and_to_different
-    errors.add(:base, 'cannot swipe yourself') if from_id == to_id
+    return unless from_id == to_id
+
+    errors.add(:base, 'cannot swipe yourself')
+    throw(:abort)
   end
 end
