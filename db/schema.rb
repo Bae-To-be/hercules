234c5718ffce5d8dc491f9f2789e5485c391b298
gauravrakheja <gauravrakheja00@gmail.com>
2021-11-15 20:43:40 +0530

    adds dummy homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_11_095851) do
+ActiveRecord::Schema.define(version: 2021_11_15_103543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
+  enable_extension "postgis"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -67,6 +68,15 @@ ActiveRecord::Schema.define(version: 2021_11_11_095851) do
     t.index ["name"], name: "index_courses_on_name", unique: true
   end
 
+  create_table "encryption_keys", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "expiry", precision: 6
+    t.string "value"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_encryption_keys_on_user_id"
+  end
+
   create_table "genders", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -107,6 +117,14 @@ ActiveRecord::Schema.define(version: 2021_11_11_095851) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["crypted_token"], name: "index_refresh_tokens_on_crypted_token", unique: true
     t.index ["user_id"], name: "index_refresh_tokens_on_user_id"
+  end
+
+  create_table "spatial_ref_sys", primary_key: "srid", id: :integer, default: nil, force: :cascade do |t|
+    t.string "auth_name", limit: 256
+    t.integer "auth_srid"
+    t.string "srtext", limit: 2048
+    t.string "proj4text", limit: 2048
+    t.check_constraint "(srid > 0) AND (srid <= 998999)", name: "spatial_ref_sys_srid_check"
   end
 
   create_table "swipes", force: :cascade do |t|
@@ -157,6 +175,8 @@ ActiveRecord::Schema.define(version: 2021_11_11_095851) do
     t.integer "search_radius", default: 0
     t.string "google_id"
     t.string "linkedin_url"
+    t.string "country_code"
+    t.string "locality"
     t.index ["company_id"], name: "index_users_on_company_id"
     t.index ["course_id"], name: "index_users_on_course_id"
     t.index ["email"], name: "index_users_on_email", unique: true
@@ -188,6 +208,7 @@ ActiveRecord::Schema.define(version: 2021_11_11_095851) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "course_relationships", "courses", column: "source_id"
   add_foreign_key "course_relationships", "courses", column: "target_id"
+  add_foreign_key "encryption_keys", "users"
   add_foreign_key "industry_relationships", "industries", column: "source_id"
   add_foreign_key "industry_relationships", "industries", column: "target_id"
   add_foreign_key "swipes", "users", column: "from_id"
@@ -223,5 +244,63 @@ ActiveRecord::Schema.define(version: 2021_11_11_095851) do
    SELECT industry_relationships.target_id AS industry_id,
       industry_relationships.source_id AS related_industry_id
      FROM industry_relationships;
+  SQL
+  create_view "geography_columns", sql_definition: <<-SQL
+      SELECT current_database() AS f_table_catalog,
+      n.nspname AS f_table_schema,
+      c.relname AS f_table_name,
+      a.attname AS f_geography_column,
+      postgis_typmod_dims(a.atttypmod) AS coord_dimension,
+      postgis_typmod_srid(a.atttypmod) AS srid,
+      postgis_typmod_type(a.atttypmod) AS type
+     FROM pg_class c,
+      pg_attribute a,
+      pg_type t,
+      pg_namespace n
+    WHERE ((t.typname = 'geography'::name) AND (a.attisdropped = false) AND (a.atttypid = t.oid) AND (a.attrelid = c.oid) AND (c.relnamespace = n.oid) AND (c.relkind = ANY (ARRAY['r'::"char", 'v'::"char", 'm'::"char", 'f'::"char", 'p'::"char"])) AND (NOT pg_is_other_temp_schema(c.relnamespace)) AND has_table_privilege(c.oid, 'SELECT'::text));
+  SQL
+  create_view "geometry_columns", sql_definition: <<-SQL
+      SELECT (current_database())::character varying(256) AS f_table_catalog,
+      n.nspname AS f_table_schema,
+      c.relname AS f_table_name,
+      a.attname AS f_geometry_column,
+      COALESCE(postgis_typmod_dims(a.atttypmod), sn.ndims, 2) AS coord_dimension,
+      COALESCE(NULLIF(postgis_typmod_srid(a.atttypmod), 0), sr.srid, 0) AS srid,
+      (replace(replace(COALESCE(NULLIF(upper(postgis_typmod_type(a.atttypmod)), 'GEOMETRY'::text), st.type, 'GEOMETRY'::text), 'ZM'::text, ''::text), 'Z'::text, ''::text))::character varying(30) AS type
+     FROM ((((((pg_class c
+       JOIN pg_attribute a ON (((a.attrelid = c.oid) AND (NOT a.attisdropped))))
+       JOIN pg_namespace n ON ((c.relnamespace = n.oid)))
+       JOIN pg_type t ON ((a.atttypid = t.oid)))
+       LEFT JOIN ( SELECT s.connamespace,
+              s.conrelid,
+              s.conkey,
+              replace(split_part(s.consrc, ''''::text, 2), ')'::text, ''::text) AS type
+             FROM ( SELECT pg_constraint.connamespace,
+                      pg_constraint.conrelid,
+                      pg_constraint.conkey,
+                      pg_get_constraintdef(pg_constraint.oid) AS consrc
+                     FROM pg_constraint) s
+            WHERE (s.consrc ~~* '%geometrytype(% = %'::text)) st ON (((st.connamespace = n.oid) AND (st.conrelid = c.oid) AND (a.attnum = ANY (st.conkey)))))
+       LEFT JOIN ( SELECT s.connamespace,
+              s.conrelid,
+              s.conkey,
+              (replace(split_part(s.consrc, ' = '::text, 2), ')'::text, ''::text))::integer AS ndims
+             FROM ( SELECT pg_constraint.connamespace,
+                      pg_constraint.conrelid,
+                      pg_constraint.conkey,
+                      pg_get_constraintdef(pg_constraint.oid) AS consrc
+                     FROM pg_constraint) s
+            WHERE (s.consrc ~~* '%ndims(% = %'::text)) sn ON (((sn.connamespace = n.oid) AND (sn.conrelid = c.oid) AND (a.attnum = ANY (sn.conkey)))))
+       LEFT JOIN ( SELECT s.connamespace,
+              s.conrelid,
+              s.conkey,
+              (replace(replace(split_part(s.consrc, ' = '::text, 2), ')'::text, ''::text), '('::text, ''::text))::integer AS srid
+             FROM ( SELECT pg_constraint.connamespace,
+                      pg_constraint.conrelid,
+                      pg_constraint.conkey,
+                      pg_get_constraintdef(pg_constraint.oid) AS consrc
+                     FROM pg_constraint) s
+            WHERE (s.consrc ~~* '%srid(% = %'::text)) sr ON (((sr.connamespace = n.oid) AND (sr.conrelid = c.oid) AND (a.attnum = ANY (sr.conkey)))))
+    WHERE ((c.relkind = ANY (ARRAY['r'::"char", 'v'::"char", 'm'::"char", 'f'::"char", 'p'::"char"])) AND (NOT (c.relname = 'raster_columns'::name)) AND (t.typname = 'geometry'::name) AND (NOT pg_is_other_temp_schema(c.relnamespace)) AND has_table_privilege(c.oid, 'SELECT'::text));
   SQL
 end
