79a8fca8c6e4213d3a1eb261105dfe53d866d79a
gauravrakheja <gauravrakheja00@gmail.com>
2021-11-24 22:16:16 +0530

    fixes rubocop violation

--- app/services/upload_image.rb
+++ app/services/upload_image.rb
@@ -8,20 +8,18 @@ class UploadImage
   end
 
   def run
-    if position.blank?
-      return ServiceResponse.bad_request('position is required')
-    end
+    return ServiceResponse.bad_request('position is required') if position.blank?
 
     ImageOptim.new.optimize_image!(path)
     existing = user.images.find_by(position: position)
     image = if existing.present?
-      existing.file.attach(path)
-      existing.tap(&:save!)
-    else
-      user.images.create!(position: position) do |record|
-        record.file.attach(path)
-      end
-    end
+              existing.file.attach(path)
+              existing.tap(&:save!)
+            else
+              user.images.create!(position: position) do |record|
+                record.file.attach(path)
+              end
+            end
     ServiceResponse.ok(image.to_h)
   rescue ActiveRecord::RecordInvalid,
          ActiveRecord::RecordNotSaved
