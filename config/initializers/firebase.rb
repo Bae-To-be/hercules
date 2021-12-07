4135543dd46d3be45770140d5373cf7d8af46e85
gauravrakheja <gauravrakheja00@gmail.com>
2021-12-07 20:13:17 +0530

    fixes rubocop violation

--- config/initializers/firebase.rb
+++ config/initializers/firebase.rb
@@ -1,4 +1,4 @@
+# frozen_string_literal: true
+
 creds = Firebase::Admin::Credentials.from_file(Rails.root.join(ENV.fetch('FIREBASE_CONFIG_PATH')))
 FIREBASE_APP = Firebase::Admin::App.new(credentials: creds)
-
-
