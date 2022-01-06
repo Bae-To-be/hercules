ef3d8a5b2a2a4e0f4256fb2d4bd2710769edea87
gauravrakheja <gauravrakheja00@gmail.com>
2022-01-06 16:00:49 +0530

    Fixes rubocop violation

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Language < ApplicationRecord
   has_many :user_languages,
            inverse_of: :language,
--- app/models/user_language.rb
+++ app/models/user_language.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class UserLanguage < ApplicationRecord
   belongs_to :user
   belongs_to :language
