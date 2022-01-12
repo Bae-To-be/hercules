36f7ade480322bead236c125c1288a71220659d1
gauravrakheja <gauravrakheja00@gmail.com>
2022-01-12 20:32:32 +0530

    adds css to fix how images look

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,4 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def resource_url(blob)
-    variant = blob.variant(resize_to_limit: [300, 300])
-    Rails.application.routes.url_helpers.rails_blob_representation_path(
-      variant.blob.signed_id, variant.variation.key, variant.blob.filename, only_path: true
-    )
-  end
 end
