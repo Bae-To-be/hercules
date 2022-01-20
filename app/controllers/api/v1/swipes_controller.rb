123153237492a894a05628809836446848373e2f
gauravrakheja <gauravrakheja00@gmail.com>
2022-01-20 16:49:10 +0530

    makes swipe API idempotent

--- app/controllers/api/v1/swipes_controller.rb
+++ app/controllers/api/v1/swipes_controller.rb
@@ -10,11 +10,19 @@ module Api
       end
 
       def create_swipe_response
-        Swipe.create!(
+        existing = Swipe.find_by(
           from_id: current_user.id,
-          to_id: params[:user_id],
-          direction: params[:direction]
+          to_id: params[:user_id]
         )
+        if existing.present?
+          existing.update(direction: params[:direction])
+        else
+          Swipe.create!(
+            from_id: current_user.id,
+            to_id: params[:user_id],
+            direction: params[:direction]
+          )
+        end
         ServiceResponse.ok(nil)
       rescue ArgumentError
         ServiceResponse.bad_request(
