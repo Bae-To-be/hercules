772ce927430ac3a952ad97d8b65410c8ebfe4a14
gauravrakheja <gauravrakheja00@gmail.com>
2022-01-24 16:08:28 +0530

    fix unread by formatting

--- app/models/message.rb
+++ app/models/message.rb
@@ -18,10 +18,10 @@ class Message < ApplicationRecord
       created_at: created_at.to_datetime.strftime('%Q'),
       updated_at: updated_at.to_datetime.strftime('%Q'),
       client_id: client_id,
-      unread_by: [
+      unread_by: {
         match_store.source_id => read_marks.to_a.detect { |mark| mark.reader_id == match_store.source_id }.nil?,
         match_store.target_id => read_marks.to_a.detect { |mark| mark.reader_id == match_store.target_id }.nil?
-      ]
+      }
     }
   end
 end
