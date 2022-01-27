10db6be4ac5fd67fb8e6544bd8d0e1b7c8bef228
gauravrakheja <gauravrakheja00@gmail.com>
2022-01-27 20:58:47 +0530

    adds length validation to report comment

--- app/models/user_report.rb
+++ app/models/user_report.rb
@@ -4,4 +4,8 @@ class UserReport < ApplicationRecord
   belongs_to :from, class_name: 'User', inverse_of: :reports_filed
   belongs_to :for, class_name: 'User', inverse_of: :reports_received
   belongs_to :user_report_reason, inverse_of: :user_reports
+
+  validates :comment,
+            length: { maximum: 500 },
+            allow_nil: true
 end
