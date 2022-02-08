1d7a5c719da4d86497658fefdaaafff9281f3516
gauravrakheja <gauravrakheja00@gmail.com>
2022-02-08 11:09:48 +0530

    fixes rubocop violation

--- spec/factories/image.rb
+++ spec/factories/image.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :image do
     association :user
-    position { user.id + Faker::Number.number(digits: 3)  }
+    position { user.id + Faker::Number.number(digits: 3) }
     file { Rack::Test::UploadedFile.new(File.open(Rails.root.join('spec/fixtures/sample.jpeg'))) }
   end
 end
